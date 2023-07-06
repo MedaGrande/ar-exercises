9057840809a2f98ae2969a7f67f7cfc72a665c25
Medhanie Alene <alenemedhanie@gmail.com>
2023-07-06 01:01:38 -0400

    establish relationship b/n store & employee models & create employess data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# employees @store1
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Andy", hourly_rate: 50)
+@store1.employees.create(first_name: "Slas", last_name: "Yemane", hourly_rate: 40)
+@store1.employees.create(first_name: "Kibrom", last_name: "Andom", hourly_rate: 30)
+@store1.employees.create(first_name: "Tsega", last_name: "Haile", hourly_rate: 25)
+
+#employess @store2
+@store2.employees.create(first_name: "Mhret", last_name: "Welday", hourly_rate: 35)
+@store2.employees.create(first_name: "Senait", last_name: "Mihretab", hourly_rate: 47)
+@store2.employees.create(first_name: "Freweini", last_name: "Girmay", hourly_rate: 37)
+@store2.employees.create(first_name: "Biniam", last_name: "Luul", hourly_rate: 15)
+@store2.employees.create(first_name: "Medido", last_name: "Grande", hourly_rate: 75)
