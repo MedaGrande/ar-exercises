0322fd138eb5e0a2dc85cebcbf6d523368d35c06
Medhanie Alene <alenemedhanie@gmail.com>
2023-07-05 23:49:45 -0400

    create three stores & instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store3 = Store.find_by(id: 3)
+puts @store1.name
+puts @store2.name
+puts @store3.name
+
+@store1 = Store.find_by(name: 'Burnaby')
+@store1.name = 'Victoria'
+@store1.save
+
+puts @store1.name
