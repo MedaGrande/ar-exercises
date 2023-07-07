3556a2c533d20ed03ff7779ac30decd05b8b42ae
Medhanie Alene <alenemedhanie@gmail.com>
2023-07-06 22:08:59 -0400

    complete exercise 7 by adding validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+store_name_input = gets.chomp.to_s
+
+store_name_input = Store.create(name: "#{store_name_input}")
+
+store_name_input.valid?
+
+puts store_name_input.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :must_have_either_mens_apparel_or_womens_apparel
+
+  def must_have_either_mens_apparel_or_womens_apparel
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:mens_apparel, "Store must have either men's apparel or women's apparel")
+      errors.add(:womens_apparel, "Store must have either men's apparel or women's apparel")
+    end 
+  end 
+
 end
